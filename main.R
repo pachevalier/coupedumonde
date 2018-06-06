39c7ad7fede078815102d83326a0c5a64bb45d57
Paul-Antoine <paul-antoine.chevalier@data.gouv.fr>
2018-06-06 18:18:40 +0200

    Compute new variables, draw the whole timeline, make a first flexdashboard

--- main.R
+++ main.R
@@ -4,8 +4,10 @@ library("dplyr")
 library("ggplot2")
 library("lubridate")
 library("ggthemes")
+library("wesanderson")
+patrick <- "([[:digit:]])\\-([[:digit:]]).*"
 
-list_tables <- read_html("https://fr.wikipedia.org/wiki/Liste_des_matchs_de_l%27%C3%A9quipe_de_France_de_football_par_adversaire") %>% 
+list_tables <- read_html("https://fr.wikipedia.org/w/index.php?title=Liste_des_matchs_de_l%27%C3%A9quipe_de_France_de_football_par_adversaire&oldid=149219728") %>% 
   html_table(fill = TRUE) 
 
 df_matches <- list_tables %>%
@@ -14,55 +16,117 @@ df_matches <- list_tables %>%
     .p = function(x) {x %>% slice(1) %>% .$X2 == "Date"}
     ) %>% 
   bind_rows() %>% 
-  filter(X2 != "Date") 
+  filter(X2 != "Date") %>%
+  select(-c(X1,X3)) %>%
+  mutate(
+    adversaire = ifelse(
+      test = grepl(pattern = "^France", x = X4), 
+      yes = sub(pattern = ".*\\-[[:blank:]]([[:alpha:]]+)", x = X4, replacement = "\\1"), 
+      no = sub(pattern = "([[:alpha:]]+)[[:blank:]]\\-.*", x = X4, replacement = "\\1")
+    ),
+    score_france = ifelse(
+      test = grepl(pattern = "^France", x = X4), 
+      yes = as.numeric(
+        sub(pattern = patrick, x = X5, replacement = "\\1")
+        ), 
+      no = as.numeric(
+        sub(pattern = patrick, x = X5, replacement = "\\2")
+      )
+    ),
+    score_adversaire = ifelse(
+      test = grepl(pattern = "^France", x = X4), 
+      yes = as.numeric(
+        sub(pattern = patrick, x = X5, replacement = "\\2")
+        ), 
+      no = as.numeric(
+        sub(pattern = patrick, x = X5, replacement = "\\1")
+      )
+    ), 
+    penalty_france = ifelse(
+      test = grepl(pattern = "^France", x = X4), 
+      yes = as.numeric(
+        sub(
+          pattern = ".*\\(([[:digit:]])\\-([[:digit:]])\\).*", 
+          replacement = "\\1", 
+          x = X5)
+        ),
+      no = as.numeric(
+        sub(
+          pattern = ".*\\(([[:digit:]])\\-([[:digit:]])\\).*", 
+          replacement = "\\2", 
+          x = X5)
+        )
+    ),
+    penalty_adversaire = ifelse(
+      test = grepl(pattern = "^France", x = X4), 
+      yes = as.numeric(
+        sub(
+          pattern = ".*\\(([[:digit:]])\\-([[:digit:]])\\).*", 
+          replacement = "\\2", 
+          x = X5)
+        ), 
+      no = as.numeric(
+        sub(
+          pattern = ".*\\(([[:digit:]])\\-([[:digit:]])\\).*", 
+          replacement = "\\1", 
+          x = X5)
+        )
+    ), 
+    date = dmy(
+      sub(
+        pattern = "([[:digit:]]{1,2}[[:blank:]][[:alpha:]]+[[:blank:]][[:digit:]]{4}).*", 
+        replacement = "\\1", 
+        x = X2)
+      ), 
+    year = year(date), 
+    outcome = case_when(
+      score_france > score_adversaire ~ "win", 
+      score_france == score_adversaire ~ "draw", 
+      TRUE ~ "loss"
+    )
+  ) %>%
+  group_by(year) %>%
+  arrange(date) %>% 
+  mutate(no = row_number()) 
+save(df_matches, file = "df_matches.Rda")
 
 df_matches %>% 
   glimpse()
 
-patrick <- "([[:digit:]])\\-([[:digit:]])"
+df_matches %>%
+  mutate(
 
-df_final <- df_matches %>%
-  select(-c(X1,X3)) %>%
-  filter(
-    grepl(pattern = "^Coupe du monde", x = X6), 
-    X5 != "-"    
     ) %>%
-  mutate(
-    score_france = ifelse(
-      test = grepl(pattern = "^France", x = X4), 
-      yes = sub(pattern = patrick, x = X5, replacement = "\\1"), 
-      no = sub(pattern = patrick, x = X5, replacement = "\\2")
-      ),
-    score_adversaire = ifelse(
-      test = grepl(pattern = "^France", x = X4), 
-      yes = sub(pattern = patrick, x = X5, replacement = "\\2"), 
-      no = sub(pattern = patrick, x = X5, replacement = "\\1")
-    ) 
-  ) %>%
-  mutate(
-    score_france = as.numeric(score_france), 
-    score_adversaire = as.numeric(score_adversaire)
-    ) %>% 
-  filter(!is.na(score_france)) %>%
-  mutate(
-    win = (score_france > score_adversaire)
-    ) %>% 
-  mutate(
-    year = as.numeric(sub(pattern = ".*([[:digit:]]{4}).*", replacement = "\\1", x = X6)), 
-    date = dmy(X2)
-    )  %>%
-  group_by(year) %>%
-  arrange(date) %>% 
-  mutate(no = row_number()) %>%
-  group_by(year) %>%
-  arrange(date) %>% 
-  mutate(no = row_number()) 
+  glimpse()
 
-df_final %>% 
-  ggplot() +
-  geom_tile(
-    mapping = aes(y = year, x = no, fill = win), 
-    color = "white"
-      ) + 
-  scale_y_discrete() + 
-  theme_fivethirtyeight()
+    
+df_matches %>%
+  group_by(outcome) %>%
+  count() %>% 
+  ggplot()
+
+
+df_matches %>% View()
+# 
+# df_matches %>% 
+#   ggplot() +
+#   geom_tile(
+#     mapping = aes(y = year, x = no, fill = outcome), 
+#     color = "white"
+#       ) + 
+#   scale_fill_manual(
+#     values = c("#879CA2", "#CB361E", "#006B97"), 
+#     labels = c("Nul", "Défaite", "Victoire"),
+#     name = "Code couleur"
+#     ) + 
+#   scale_y_reverse(
+#     name = "Année"
+#   ) + 
+#   theme_fivethirtyeight() + 
+#   theme(
+#     rect = element_blank(), 
+#     legend.position = "bottom"
+#     ) + 
+#   coord_equal()
+# 
+# library(tweenr)
